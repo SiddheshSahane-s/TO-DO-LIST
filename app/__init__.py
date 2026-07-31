eb31ef597f1547c3ea62bb968af5505baf123258
SiddheshSahane-s <shrijadhav18110@gmail.com>
2026-07-31 22:43:39 +0530

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+#setupping app factory in this file, create,addinng configurations, register blueprint,initialize database
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() #created database
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = "MODI"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    db.init_app(app)
+    
+    from app.routes.auth import auth_bp
+    from app.routes.tasks import task_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(task_bp)
+    
+    return app
---
+++ app/routes/auth.py
@@ -0,0 +1,29 @@
+#consist login logout routes
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+
+auth_bp = Blueprint('auth', __name__)
+
+USER_CREDENTIALS = {
+    'username':"admin",
+    'password':"123"
+}
+@auth_bp.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        if username == USER_CREDENTIALS["username"] and password == USER_CREDENTIALS["password"]:
+            session['user'] = username
+            flash("login successful", "success")
+            return redirect(url_for("tasks.view_tasks"))
+        else:
+            flash("invalid username or  password", 'danger')
+            
+    return render_template("login.html")
+
+@auth_bp.route('/logout')
+def logout():
+    session.pop('user', None)
+    flash('logged out', 'info')
+    return redirect(url_for('auth.login'))
